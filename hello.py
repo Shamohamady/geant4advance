ef35da8580822ae936005cfbe8df16ef77f64964
shamohamady <rasul.shamohamady@gmail.com>
2020-12-20 09:33:51 +0330

    as

---
+++ hello.py
@@ -0,0 +1,11 @@
+import HellowMe
+import sys 
+HellowMe.greeting("Jonathan")
+print(sys.version)
+for item in  range(2,10,2) :
+    print('hellow word!' ) 
+    print(item)
+else:
+    thislist = ["apple", "banana", "cherry"]
+    thislist[1] = "blackcurrant"
+    print(thislist)
